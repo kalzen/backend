a073e37752e95a227c807039183d5b04ac6637b8
kalzen <manhto109@gmail.com>
2025-03-29 14:10:28 +0700

    update build

---
+++ public/build/assets/input-error-BjMAVeQF.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BfpfXKls.js";import{c as o}from"./app-logo-icon-CmB2uMge.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
